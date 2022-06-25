8faffd62d31060c2486a751c5cb73b6d46f2f2c6
pingu@benjamin-herb.de <pingu@benjamin-herb.de>
2022-06-25 17:20:17 +0200

    First running version

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 import os
+import time
+import subprocess
+import platform
+import multiprocessing
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog as fd
+
+from . import ffmpeg_commands
 
 
 class LookbookComparisonCreatorApp(tk.Tk):
@@ -9,10 +15,9 @@ class LookbookComparisonCreatorApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Lookbook Comparison Creator")
-
         # Place Window Centered
         width = 450
-        height = 160
+        height = 190
         width_screen = self.winfo_screenwidth()
         height_screen = self.winfo_screenheight()
         x_win = (width_screen/2) - (width/2)
@@ -23,12 +28,14 @@ class LookbookComparisonCreatorApp(tk.Tk):
         self.maxsize(width=width + 200, height=height)
 
         self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.frame = ttk.Frame(padding='10')
         self.frame.columnconfigure(1, weight=1)
+        self.frame.rowconfigure(3, weight=1)
         self.frame.grid(sticky="NEWS")
 
         # FFMPEG
-        self.ffmpeg_cmd = tk.StringVar(self)
+        self.ffmpeg_cmd = tk.StringVar(self, 'ffmpeg')
         self.ffmpeg_label = ttk.Label(self.frame, text="FFmpeg:")
         self.ffmpeg_label.grid(row=0, column=0, padx=5, pady=5, sticky="W")
 
@@ -45,7 +52,7 @@ class LookbookComparisonCreatorApp(tk.Tk):
                 )
 
         # SOURCE DIRECTORY
-        self.source_dir = tk.StringVar(self)
+        self.source_dir = tk.StringVar(self, '~')
         self.source_label = ttk.Label(self.frame, text="Source:")
         self.source_label.grid(row=1, column=0, padx=5, pady=5, sticky="W")
         self.source_entry = ttk.Entry(
@@ -60,8 +67,8 @@ class LookbookComparisonCreatorApp(tk.Tk):
                 )
 
         # COMPARISON STRING
-        self.key_string = tk.StringVar(self)
-        self.key_label = ttk.Label(self.frame, text="Keys:", width=6)
+        self.key_string = tk.StringVar(self, 'Key,Close,Orbiter|Robe,°,-Frost')
+        self.key_label = ttk.Label(self.frame, text="Keys:")
         self.key_label.grid(row=2, column=0, padx=5, pady=5, sticky="W")
         self.key_entry = ttk.Entry(
                 self.frame, justify="left", textvariable=self.key_string)
@@ -70,31 +77,75 @@ class LookbookComparisonCreatorApp(tk.Tk):
 
         # RUN
         self.run_button = ttk.Button(
-        self, text="Create Quicktime", command=self.run)
-        self.run_button.grid(row=0, column=3, sticky="ES", padx=5, pady=5)
+            self.frame, text="Create Video", command=self.run)
+        self.run_button.grid(row=3, column=1, sticky="EWS", padx=5, pady=5)
 
     def select_source(self):
-        initialdir = os.path.dirname(self.ffmpeg_cmd.get())
+        initialdir = os.path.dirname(self.source_dir.get())
         new_source = fd.askdirectory(
             title='Choose source file', initialdir=initialdir)
         if new_source:
             self.source_dir.set(new_source)
 
     def select_ffmpeg(self):
-        initialdir = os.path.dirname(self.ffmpeg_cmd.get())
+        initialdir = self.ffmpeg_cmd.get()
         new_ffmpeg_cmd = fd.askopenfilename(
-            title='Choose source file', initialdir=initialdir)
+            title='Choose ffmpeg binary', initialdir=initialdir)
         if new_ffmpeg_cmd:
             self.ffmpeg_cmd.set(new_ffmpeg_cmd)
 
-
     def run(self):
+        key = self.key_string.get().split(',')
+        initial_filename = f"{('_'.join(key)).replace(' ', '_').replace('|', '+')}"
+        self.toggle_ui(state='disable')
+        output_file = fd.asksaveasfilename(
+                initialdir='~', initialfile=initial_filename,
+                filetypes=[('Video Files', '*.mp4'), ('All Files', '*')])
 
-    
-def main():
-    app = LookbookComparisonCreatorApp()
-    app.mainloop()
+        # if it was canceled
+        if output_file == "":
+            return
 
+        self.render_process = multiprocessing.Process(
+                target=ffmpeg_commands.create_comparison_clips,
+                args=(
+                    self.ffmpeg_entry.get(), key,
+                    self.source_dir.get(), output_file))
+        self.render_process.start()
 
-if __name__ == "__main__":
-    main()
+        while self.render_process.is_alive():
+            self.update()
+            time.sleep(1)
+            print("RUNNING")
+
+        self.render_process.join()
+        self.toggle_ui('enable')
+
+        # Try to open finished video
+        try:
+            if platform.system() == 'Darwin':       # macOS
+                subprocess.call(('open', output_file))
+            elif platform.system() == 'Windows':    # Windows
+                os.startfile(output_file)
+            else:                                   # linux variants
+                subprocess.call(('xdg-open', output_file))
+        except Exception as e:
+            print(f"FAILED TO OPEN {e}")
+
+    def cancel(self):
+        if self.render_process.is_alive():
+            self.render_process.terminate()
+
+    def toggle_ui(self, state='disable'):
+        self.ffmpeg_entry.configure(state=state)
+        self.choose_ffmpeg_button.configure(state=state)
+        self.source_entry.configure(state=state)
+        self.choose_source_button.configure(state=state)
+        self.key_entry.configure(state=state)
+
+        if state == 'disable':
+            self.run_button.configure(
+                    text='Cancel', command=self.cancel)
+        else:
+            self.run_button.configure(
+                    text='Create Video', command=self.run)
--- src/ffmpeg_commands.py
+++ src/ffmpeg_commands.py
@@ -3,46 +3,47 @@ import math
 import re
 import subprocess
 
-OCF_DIR = "540"
 OUTPUT_RESOLUTION = "1920:1080"
 INPUT_RESOLUTION = "960:540"
 OUTPUT_DIR = "out_update"
 
-KEYS = [
-        ['Key', 'Close', 'Orbiter|Robe', '°', '-Frost'],
-#        [''],
- #       ['Kicker'],
- #       ['Key', 'Close'],
- #       ['Key', 'Wide'],
- #       ['Key', 'Close', 'Orbiter'],
- #       ['Key', 'Wide', 'Orbiter'],
- #       ['Kicker', 'Orbiter'],
-#        ['Key', 'Close', 'Maxi Mix'],
- #       ['Key', 'Wide', 'Maxi Mix'],
- #       ['Kicker', 'Maxi Mix'],
- #       ['Key', 'Close', 'Mini Mix'],
- #       ['Key', 'Wide', 'Mini Mix'],
- #       ['Kicker', 'Mini Mix'],
- #       ['Key', 'Close', 'Astera'],
- #       ['Key', 'Wide', 'Astera'],
- #       ['Kicker', 'Astera'],
- #       ['Key', 'Close', 'LIGHTBRIDGE'],
- #       ['Key', 'Wide', 'LIGHTBRIDGE'],
- #       ['Kicker', 'LIGHTBRIDGE'],
- #       ['Key', 'Close', 'Robe'],
- #       ['Key', 'Wide', 'Robe'],
- #       ['Kicker', 'Robe'],
- #       ['Key', 'Close', 'SL1'],
- #       ['Key', 'Wide', 'SL1'],
- #       ['Kicker', 'SL1'],
- #       ['Key', 'Close', 'Dash Light'],
- #       ['Key', 'Wide', 'Dash Light'],
-#        ['Kicker', 'Dash Light'],
-        ]
+DEFAULT_KEYS = [
+    ['Key', 'Close', 'Orbiter|Robe', '°', '-Frost'],
+    [''],
+    ['Kicker'],
+    ['Key', 'Close'],
+    ['Key', 'Wide'],
+    ['Key', 'Close', 'Orbiter'],
+    ['Key', 'Wide', 'Orbiter'],
+    ['Kicker', 'Orbiter'],
+    ['Key', 'Close', 'Maxi Mix'],
+    ['Key', 'Wide', 'Maxi Mix'],
+    ['Kicker', 'Maxi Mix'],
+    ['Key', 'Close', 'Mini Mix'],
+    ['Key', 'Wide', 'Mini Mix'],
+    ['Kicker', 'Mini Mix'],
+    ['Key', 'Close', 'Astera'],
+    ['Key', 'Wide', 'Astera'],
+    ['Kicker', 'Astera'],
+    ['Key', 'Close', 'LIGHTBRIDGE'],
+    ['Key', 'Wide', 'LIGHTBRIDGE'],
+    ['Kicker', 'LIGHTBRIDGE'],
+    ['Key', 'Close', 'Robe'],
+    ['Key', 'Wide', 'Robe'],
+    ['Kicker', 'Robe'],
+    ['Key', 'Close', 'SL1'],
+    ['Key', 'Wide', 'SL1'],
+    ['Kicker', 'SL1'],
+    ['Key', 'Close', 'Dash Light'],
+    ['Key', 'Wide', 'Dash Light'],
+    ['Kicker', 'Dash Light'],
+]
 
-files = os.listdir(OCF_DIR)
 
-for key in KEYS:
+def create_comparison_clips(ffmpeg_cmd, key, source_dir, output_file):
+
+    files = os.listdir(source_dir)
+
     input_files = []
     max_files = []
     # Check for matching input files
@@ -77,12 +78,12 @@ for key in KEYS:
     print()
 
     ffmpeg_cmd = [
-            "ffmpeg",
+            ffmpeg_cmd,
             ]
 
     for input in input_files:
         ffmpeg_cmd.append("-i")
-        ffmpeg_cmd.append(f"{OCF_DIR}/{input}")
+        ffmpeg_cmd.append(f"{source_dir}/{input}")
 
     # calculate the best grid for the video count
     squared = round(math.sqrt(len(input_files)))
@@ -117,15 +118,24 @@ for key in KEYS:
     ffmpeg_cmd.extend([
         "-filter_complex", down_scale_string + filter_string,
         "-map", "[v]",
-#        "-y",
+        "-y",
         "-c:v", "libx264",
         "-crf", "18",
         "-pix_fmt", "yuv420p",
         "-loglevel", "warning",
         "-stats",
-        f"{OUTPUT_DIR}/{('_'.join(key)).replace(' ', '_').replace('|', '+')}.mov"
+        output_file
         ])
 
     print(f"FFMPEG CMD: \n{ffmpeg_cmd}")
 
-    subprocess.run(ffmpeg_cmd)
+    run_output = subprocess.run(ffmpeg_cmd, capture_output=True)
+    print(run_output)
+
+
+if __name__ == '__main__':
+
+    source_dir = "540"
+    for key in DEFAULT_KEYS:
+        output_file = f"{OUTPUT_DIR}/{('_'.join(key)).replace(' ', '_').replace('|', '+')}.mov"
+        create_comparison_clips(key, source_dir, output_file)
